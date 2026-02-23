4dd3d17dfb81ca80a89a3a5a6877c9b41962e597
Avishai Dernis <avid29@live.com>
2026-02-23 22:02:06 +0200

    Added new demo project "MathLib"

---
+++ demos/MathLib/main.asm
@@ -0,0 +1,21 @@
+.text
+.globl square_func   # Reference external symbol
+
+entry:
+    
+    # Read int
+    li      $v0,    5
+    syscall
+
+    # Take input squred
+    move    $a0,    $v0         
+    jal     square_func   
+    
+    # Print the input squared
+    move    $a0,    $v0
+    li      $v0,    1
+    syscall
+    
+    # Exit gracefully
+    li      $v0,    10
+    syscall
---
+++ demos/MathLib/math_lib.asm
@@ -0,0 +1,7 @@
+.text
+.globl square_func    # Export symbol
+
+square_func:
+    mult $a0, $a0     # Multiply arg by itself
+    mflo $v0          # Move result to return register
+    jr $ra            # Return
