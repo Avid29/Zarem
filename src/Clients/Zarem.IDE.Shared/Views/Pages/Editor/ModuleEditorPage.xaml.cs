09c250ca33f8a81babf7cf1fdc7f7f400f1fbaa4
Adam Dernis <avid29@live.com>
2026-03-07 23:11:50 +0200

    WIP view-only ModuleEditorPage

--- src/Clients/Zarem.IDE.Shared/Views/Pages/Editor/ModuleEditorPage.xaml.cs
+++ src/Clients/Zarem.IDE.Shared/Views/Pages/Editor/ModuleEditorPage.xaml.cs
@@ -1,14 +1,21 @@
 // Avishai Dernis 2025
 
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System.Threading.Tasks;
+using Zarem.Elf;
 using Zarem.IDE.ViewModels.Pages;
 using Zarem.IDE.ViewModels.Pages.Interfaces;
+using Zarem.Models;
+using Zarem.Models.Tables;
 
 namespace Zarem.IDE.Views.Pages.Editor;
 
 public sealed partial class ModuleEditorPage : UserControl, IFileEditorHandler
 {
+    public static readonly DependencyProperty ModuleProperty =
+        DependencyProperty.Register(nameof(Module), typeof(Module), typeof(ModuleEditorPage), new PropertyMetadata(null));
+
     /// <summary>
     /// Initializes a new instance of the <see cref="ModuleEditorPage"/> class.
     /// </summary>
@@ -34,15 +41,31 @@ public sealed partial class ModuleEditorPage : UserControl, IFileEditorHandler
     /// <inheritdoc/>
     public bool IsDirty => false;
 
+    public Module? Module
+    {
+        get => (Module?)GetValue(ModuleProperty);
+        set => SetValue(ModuleProperty, value);
+    }
+
+    public static string FormatOffset(long offset) => $"0x{offset:X8}";
+
     /// <inheritdoc/>
     public async Task<bool> SaveAsync()
     {
         // TODO: Save module file
+        // For now it's view-only
+
         return false;
     }
 
     private async Task LoadContentAsync()
     {
-        // TODO: Parse and load module file
+        var file = ViewModel?.File;
+        if (file is null)
+            return;
+
+        // TODO: Dynamically identify the module type
+        var stream = await file.FileItem.OpenStreamForReadAsync();
+        Module = ElfModule.Open(file.Name, stream)?.Abstract(new());
     }
 }
