60774523a830c8fbdd586265b13659dd47999f8d
Adam Dernis <avid29@live.com>
2026-03-13 08:30:32 +0200

    Simplified EditorOperation commands for TitleBar

--- src/Clients/Zarem.IDE.ViewModels/ViewModels/WindowViewModel/WindowViewModel.Commands.cs
+++ src/Clients/Zarem.IDE.ViewModels/ViewModels/WindowViewModel/WindowViewModel.Commands.cs
@@ -53,48 +53,7 @@ public partial class WindowViewModel
     #region Edit
 
     [RelayCommand]
-    private void Undo() => SendEdit(EditorOperation.Undo);
-
-    [RelayCommand]
-    private void Redo() => SendEdit(EditorOperation.Redo);
-
-    [RelayCommand]
-    private void Cut() => SendEdit(EditorOperation.Cut);
-
-    [RelayCommand]
-    private void Copy() => SendEdit(EditorOperation.Copy);
-
-    [RelayCommand]
-    private void Paste() => SendEdit(EditorOperation.Paste);
-
-    [RelayCommand]
-    private void Duplicate() => SendEdit(EditorOperation.Duplicate);
-
-    [RelayCommand]
-    private void SelectAll() => SendEdit(EditorOperation.SelectAll);
-
-    [RelayCommand]
-    private void TransposeUp() => SendEdit(EditorOperation.TransposeUp);
-
-    [RelayCommand]
-    private void TransposeDown() => SendEdit(EditorOperation.TransposeDown);
-
-    [RelayCommand]
-    private void ToggleOutlining() => SendEdit(EditorOperation.ToggleOutlining);
-
-    [RelayCommand]
-    private void ExpandChildren() => SendEdit(EditorOperation.ExpandChildren);
-
-    [RelayCommand]
-    private void CollapseChildren() => SendEdit(EditorOperation.CollapseChildren);
-
-    [RelayCommand]
-    private void ExpandAll() => SendEdit(EditorOperation.ExpandAll);
-
-    [RelayCommand]
-    private void CollapseAll() => SendEdit(EditorOperation.CollapseAll);
-
-    private void SendEdit(EditorOperation operation) => _messenger.Send(new EditorOperationRequestMessage(operation));
+    private void SendEditOperation(EditorOperation operation) => _messenger.Send(new EditorOperationRequestMessage(operation));
 
     #endregion
 
