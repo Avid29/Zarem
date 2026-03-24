0b32bcda8d17cb072ba7a8e47c79efbc8531894d
Avishai Dernis <avid29@live.com>
2026-03-24 03:46:41 +0200

    Added IGraphicsDevice and ZaremGBU

---
+++ src/Zarem.Emulator/Machine/Devices/Interfaces/IGraphicsDevice.cs
@@ -0,0 +1,23 @@
+﻿// Avishai Dernis 2026
+
+using CommunityToolkit.HighPerformance;
+using System;
+using Zarem.Emulator.Machine.Interfaces;
+
+namespace Zarem.Emulator.Machine.Devices.Interfaces;
+
+/// <summary>
+/// An <see cref="IBusDevice"/> for a graphics device.
+/// </summary>
+public interface IGraphicsDevice
+{
+    /// <summary>
+    /// An event invoked when the graphics device signals a refresh.
+    /// </summary>
+    event EventHandler? Refresh;
+
+    /// <summary>
+    /// Gets the pixel buffer for graphics device.
+    /// </summary>
+    ReadOnlySpan2D<uint> GetPixelBuffer();
+}
