8e071ecffd17c999e4e0c8645c695aac4a25534c
Avishai Dernis <avid29@live.com>
2026-03-25 23:16:54 +0200

    Added SpimTrapHandler and descriptor

---
+++ src/Archs/MIPS/Zarem.MIPS/TrapHandlers/SpimTrapHandlerDescriptor.cs
@@ -0,0 +1,21 @@
+﻿// Avishai Dernis 2026
+
+using System;
+using Zarem.Attributes;
+using Zarem.Descriptors;
+using Zarem.Emulator.TrapHandlers;
+
+namespace Zarem.MIPS.TrapHandlers;
+
+/// <summary>
+/// An <see cref="ITrapHandlerDescriptor"/> for the <see cref="SpimTrapHandler"/>.
+/// </summary>
+[ZaremPlugin]
+public class SpimTrapHandlerDescriptor : ITrapHandlerDescriptor
+{
+    /// <inheritdoc/>
+    public string Identifier => "SPIM";
+
+    /// <inheritdoc/>
+    public Type Type => typeof(SpimTrapHandler);
+}
