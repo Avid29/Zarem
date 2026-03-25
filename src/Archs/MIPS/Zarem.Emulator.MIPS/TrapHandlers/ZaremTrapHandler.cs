1bb80d9541a0db198e9d2a470bfcfc4303317771
Avishai Dernis <avid29@live.com>
2026-03-25 22:57:30 +0200

    Moved syscalls to helper functions

--- src/Archs/MIPS/Zarem.Emulator.MIPS/TrapHandlers/ZaremTrapHandler.cs
+++ src/Archs/MIPS/Zarem.Emulator.MIPS/TrapHandlers/ZaremTrapHandler.cs
@@ -2,8 +2,7 @@
 
 using System;
 using System.Text;
-using Zarem.Emulator.Extensions;
-using Zarem.Models.Instructions.Enums.Registers;
+using Zarem.Emulator.TrapHandlers.Base;
 
 namespace Zarem.Emulator.TrapHandlers;
 
@@ -17,96 +16,18 @@ public class ZaremTrapHandler : MipsTrapHandler
     {
         switch (code)
         {
-            // Print integer
-            case 1:
-                Console.Write($"{context.A0}");
-                break;
-
-            // Print float
-            case 2:
-                Console.Write($"{context.Cpu.FloatProcessor.Singles[FloatRegister.F12]}");
-                break;
-
-            // Print double
-            case 3:
-                Console.Write($"{context.Cpu.FloatProcessor.Doubles[FloatRegister.F12]}");
-                break;
-
-            // Print ascii string
-            case 4:
-                Console.Write(context.Cpu.Memory.ReadString(context.A0, Encoding.ASCII));
-                break;
-
-            // Read integer
-            case 5:
-                context.V0 = (uint)int.Parse(Console.ReadLine() ?? "");
-                break;
-
-            // Read float
-            case 6:
-                context.Cpu.FloatProcessor.Singles[FloatRegister.F0] = float.Parse(Console.ReadLine() ?? "");
-                break;
-
-            // Read double
-            case 7:
-                context.Cpu.FloatProcessor.Doubles[FloatRegister.F0] = double.Parse(Console.ReadLine() ?? "");
-                break;
-
-            // Read ascii string
-            case 8:
-                context.Cpu.Memory.Write(context.A0, ReadString(Encoding.ASCII, context.A1));
-                break;
-
-            // Stop execution
-            case 10:
-                context.Cpu.RequestShutdown();
-                break;
-
-            // Print unicode string
-            case 80:
-                Console.Write(context.Cpu.Memory.ReadString(context.A0, Encoding.BigEndianUnicode));
-                break;
-
-            // Read unicode string
-            case 81:
-                context.Cpu.Memory.Write(context.A0, ReadString(Encoding.BigEndianUnicode, context.A1));
-                break;
-
-            default:
-                throw new NotImplementedException();
+            case 1:     PrintInteger(context); break;
+            case 2:     PrintFloat(context); break;
+            case 3:     PrintDouble(context); break;
+            case 4:     PrintString(context, Encoding.ASCII); break;
+            case 5:     ReadInteger(context); break;
+            case 6:     ReadFloat(context); break;
+            case 7:     ReadDouble(context); break;
+            case 8:     ReadString(context, Encoding.ASCII); break;
+            case 10:    Shutdown(context); break;
+            case 80:    PrintString(context, Encoding.BigEndianUnicode); break;
+            case 81:    ReadString(context, Encoding.BigEndianUnicode); break;
+            default:    throw new NotImplementedException();
         }
-
-        // Increment the PC
-        context.Cpu.ProgramCounter += 4;
-    }
-
-    private static byte[] ReadString(Encoding encoding, uint maxBytes)
-    {
-        var str = Console.ReadLine() ?? string.Empty;
-
-        // Determine null-terminator size (1 for UTF8, 2 for UTF16, etc.)
-        int stride = encoding.GetByteCount("\0");
-
-        // We must leave 'stride' bytes at the end for the \0
-        int maxDataBytes = (int)maxBytes - stride;
-        if (maxDataBytes < 0)
-            return [];
-
-        byte[] bytes = new byte[maxBytes];
-        var encoder = encoding.GetEncoder();
-
-        // Apply encoding
-        encoder.Convert(
-            chars: str.AsSpan(),
-            bytes: bytes.AsSpan(0, maxDataBytes),
-            flush: true,
-            out int _,
-            out int bytesUsed,
-            out bool _);
-
-        // Apply null terminator and resize the array to only the bytes used.
-        Array.Resize(ref bytes, bytesUsed + stride);
-        bytes.AsSpan(bytesUsed, stride).Clear();
-        return bytes;
     }
 }
