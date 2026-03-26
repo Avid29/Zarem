9a339d3c85201adad1ca4fd547971563e19da32c
Avishai Dernis <avid29@live.com>
2026-03-26 22:46:50 +0200

    Cleared up MIPS versioning enum

--- src/Archs/MIPS/Zarem.MIPS.Base/Models/Instructions/Enums/MipsVersion.cs
+++ src/Archs/MIPS/Zarem.MIPS.Base/Models/Instructions/Enums/MipsVersion.cs
@@ -14,18 +14,21 @@ public enum MipsVersion : byte
 #pragma warning disable CS1591
 
     // --- The Classic Era (MIPS I - V) ---
-    [XmlEnum("mips1")] [JsonStringEnumMemberName("MipsI")] MipsI = 1,   // Baseline (R2000/R3000)
-    [XmlEnum("mips2")] [JsonStringEnumMemberName("MipsII")] MipsII = 2,  // Added Traps, LL/SC, Likely Branches
-    [XmlEnum("mips3")] [JsonStringEnumMemberName("MipsIII")] MipsIII = 3, // 64-bit support (R4000)
-    [XmlEnum("mips4")] [JsonStringEnumMemberName("MipsIV")] MipsIV = 4,  // Added MOVZ/MOVN, Floating Point MADD
-    [XmlEnum("mips5")] [JsonStringEnumMemberName("MipsV")] MipsV = 5,   // Paired-Single Floats (Rarely used)
+    [XmlEnum("mips1")] [JsonStringEnumMemberName("MipsI")] MipsI = 1,
+    [XmlEnum("mips2")] [JsonStringEnumMemberName("MipsII")] MipsII = 2,
+    [XmlEnum("mips3")] [JsonStringEnumMemberName("MipsIII")] MipsIII = 3,
+    [XmlEnum("mips4")] [JsonStringEnumMemberName("MipsIV")] MipsIV = 4,
+    [XmlEnum("mips5")] [JsonStringEnumMemberName("MipsV")] MipsV = 5,
 
-    // --- The Standardized Era ---
-    [XmlEnum("mips32r1")] [JsonStringEnumMemberName("Mips32R1")] Mips32R1 = 10, // Baseline for modern 32-bit (includes MUL, CLZ)
-    [XmlEnum("mips32r2")] [JsonStringEnumMemberName("Mips32R2")] Mips32R2 = 11, // Added EXT/INS (Bitfield ops)
+    // --- The MIPS 32/64 Era ---
+    [XmlEnum("mips32r1")] [JsonStringEnumMemberName("Mips32R1")] Mips32R1 = 11,
+    [XmlEnum("mips32r2")] [JsonStringEnumMemberName("Mips32R2")] Mips32R2 = 12,
+    [XmlEnum("mips32r3")] [JsonStringEnumMemberName("Mips32R3")] Mips32R3 = 13,
+    // There is no R4
+    [XmlEnum("mips32r5")] [JsonStringEnumMemberName("Mips32R5")] Mips32R5 = 15,
 
     // --- The "Breaking" Era ---
-    [XmlEnum("mips32r6")] [JsonStringEnumMemberName("Mips32R6")] Mips32R6 = 16  // REMOVED Delay Slots, reorganized opcodes
+    [XmlEnum("mips32r6")] [JsonStringEnumMemberName("Mips32R6")] Mips32R6 = 16,
 
 #pragma warning restore CS1591
 }
