80885392f52c26fa1efe51f34adba78cbc96b2f9
Avishai Dernis <avid29@live.com>
2026-03-28 23:59:02 +0300

    Added hi/lo to register table

--- src/Archs/MIPS/Zarem.Assembler.MIPS/Helpers/Tables/RegistersTable.cs
+++ src/Archs/MIPS/Zarem.Assembler.MIPS/Helpers/Tables/RegistersTable.cs
@@ -89,17 +89,13 @@ public static class RegistersTable
     private static readonly Dictionary<string, GPRegister> _gpRegisterTable = new()
     {
         { "zero", GPRegister.Zero },
-
         { "at", GPRegister.AssemblerTemporary },
-
         { "v0", GPRegister.ReturnValue0 },
         { "v1", GPRegister.ReturnValue1 },
-
         { "a0", GPRegister.Argument0 },
         { "a1", GPRegister.Argument1 },
         { "a2", GPRegister.Argument2 },
         { "a3", GPRegister.Argument3 },
-
         { "t0", GPRegister.Temporary0 },
         { "t1", GPRegister.Temporary1 },
         { "t2", GPRegister.Temporary2 },
@@ -108,7 +104,6 @@ public static class RegistersTable
         { "t5", GPRegister.Temporary5 },
         { "t6", GPRegister.Temporary6 },
         { "t7", GPRegister.Temporary7 },
-
         { "s0", GPRegister.Saved0 },
         { "s1", GPRegister.Saved1 },
         { "s2", GPRegister.Saved2 },
@@ -117,17 +112,15 @@ public static class RegistersTable
         { "s5", GPRegister.Saved5 },
         { "s6", GPRegister.Saved6 },
         { "s7", GPRegister.Saved7 },
-
         { "t8", GPRegister.Temporary8 },
         { "t9", GPRegister.Temporary9 },
-
         { "k0", GPRegister.Kernel0 },
         { "k1", GPRegister.Kernel1 },
-
         { "gp", GPRegister.GlobalPointer },
         { "sp", GPRegister.StackPointer },
         { "fp", GPRegister.FramePointer },
-
         { "ra", GPRegister.ReturnAddress },
+        { "hi", GPRegister.High },
+        { "lo", GPRegister.Low },
     };
 }
