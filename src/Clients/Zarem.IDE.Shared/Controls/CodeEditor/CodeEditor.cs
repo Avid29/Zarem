6b4781f7d6ec930ac8cf877769f8fadb0f8f3ce2
Avishai Dernis <avid29@live.com>
2026-03-30 07:55:01 +0300

    Restored position address feature

--- src/Clients/Zarem.IDE.Shared/Controls/CodeEditor/CodeEditor.cs
+++ src/Clients/Zarem.IDE.Shared/Controls/CodeEditor/CodeEditor.cs
@@ -9,9 +9,10 @@ using Zarem.Assembler;
 using Zarem.Assembler.Config;
 using Zarem.Assembler.Handlers;
 using Zarem.Assembler.Logging;
+using Zarem.Assembler.Tokenization.Models;
 using Zarem.Helpers;
-using Zarem.IDE.Controls.CodeEditor.Scintilla;
 using Zarem.IDE.Services.Settings.Enums;
+using Zarem.Models;
 using Zarem.Models.Breakpoints;
 using Zarem.Models.Instructions.Enums;
 using Zarem.Models.Tables;
@@ -21,14 +22,16 @@ namespace Zarem.IDE.Controls.CodeEditor;
 [TemplatePart(Name = ICodeEditorPartName, Type = typeof(ICodeEditor))]
 public sealed partial class CodeEditor : Control, ICodeEditor
 {
+    private TokenizedAssembly? _tokenizedAssembly;
+
     public static readonly DependencyProperty TextProperty =
         DependencyProperty.Register(nameof(Text), typeof(string), typeof(CodeEditor), new PropertyMetadata(string.Empty, OnTextPropertyChanged));
 
     public static readonly DependencyProperty LineProperty =
-        DependencyProperty.Register(nameof(Line), typeof(long), typeof(CodeEditor), new PropertyMetadata(0L));
+        DependencyProperty.Register(nameof(Line), typeof(long), typeof(CodeEditor), new PropertyMetadata(0L, OnPositionPropertyChanged));
 
     public static readonly DependencyProperty ColumnProperty =
-        DependencyProperty.Register(nameof(Column), typeof(long), typeof(CodeEditor), new PropertyMetadata(0L));
+        DependencyProperty.Register(nameof(Column), typeof(long), typeof(CodeEditor), new PropertyMetadata(0L, OnPositionPropertyChanged));
 
     public static readonly DependencyProperty ZoomProperty =
         DependencyProperty.Register(nameof(Zoom), typeof(int), typeof(CodeEditor), new PropertyMetadata(100));
@@ -41,6 +44,9 @@ public sealed partial class CodeEditor : Control, ICodeEditor
 
     public static readonly DependencyProperty AnnotationThresholdProperty =
         DependencyProperty.Register(nameof(AnnotationThreshold), typeof(AnnotationThreshold), typeof(CodeEditor), new PropertyMetadata(AnnotationThreshold.Errors, OnLogAnnotationsChanged));
+
+    public static readonly DependencyProperty PositionAddressProperty =
+        DependencyProperty.Register(nameof(PositionAddress), typeof(Address?),typeof(CodeEditor), new PropertyMetadata(null));
 
     private const string ICodeEditorPartName = "PART_ICodeEditor";
 
@@ -107,6 +113,12 @@ public sealed partial class CodeEditor : Control, ICodeEditor
         set => SetValue(AnnotationThresholdProperty, value);
     }
 
+    public Address? PositionAddress
+    {
+        get => (Address?)GetValue(PositionAddressProperty);
+        set => SetValue(PositionAddressProperty, value);
+    }
+
     public SymbolResolver? SymbolResolver { get; private set; }
 
     public void NavigateToToken(SourceLocation location) => _codeEditor?.NavigateToToken(location);
@@ -118,6 +130,10 @@ public sealed partial class CodeEditor : Control, ICodeEditor
 
     /// <inheritdoc/>
     public void ClearLogHighlights() => _codeEditor?.ClearLogHighlights();
+
+    public void RegisterBreakpointSource(BreakpointCollection breakpoints) => _codeEditor?.RegisterBreakpointSource(breakpoints);
+
+    public void UnregisterBreakpointSource() => _codeEditor?.UnregisterBreakpointSource();
 
     public static void OnTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
@@ -148,6 +164,14 @@ public sealed partial class CodeEditor : Control, ICodeEditor
         _ = codeEditor.RunAssemblerAsync();
     }
 
+    private static void OnPositionPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+    {
+        if (d is not CodeEditor codeEditor)
+            return;
+
+        codeEditor.UpdatePositionAddress();
+    }
+
     private async Task RunAssemblerAsync()
     {
         if (!RealTimeAssembly)
@@ -159,6 +183,7 @@ public sealed partial class CodeEditor : Control, ICodeEditor
             var config = new MipsAssemblerConfig(MipsVersion.MipsIII);
             var result = await Zarembler.AssembleAsync(Text, "editor", new MipsAssmblerHandler(config), config);
             SymbolResolver = new SymbolResolver(result.Symbols);
+            _tokenizedAssembly = result.Tokens;
 
             ApplyLogHighlights(result.Logs);
         }
@@ -168,7 +193,13 @@ public sealed partial class CodeEditor : Control, ICodeEditor
         }
     }
 
-    public void RegisterBreakpointSource(BreakpointCollection breakpoints) => _codeEditor?.RegisterBreakpointSource(breakpoints);
+    private void UpdatePositionAddress()
+    {
+        if (_tokenizedAssembly is null || _tokenizedAssembly.LineCount is 0)
+            return;
 
-    public void UnregisterBreakpointSource() => _codeEditor?.UnregisterBreakpointSource();
+        var line = Math.Min(Line + 1, _tokenizedAssembly.LineCount - 1);
+        var asmLine = _tokenizedAssembly[(int)line];
+        PositionAddress = asmLine.Address;
+    }
 }
