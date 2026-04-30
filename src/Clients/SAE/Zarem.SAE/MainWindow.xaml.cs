f89c81e9dfad2867130b20f760eca32d88f15a57
Avishai Dernis <avid29@live.com>
2026-04-30 13:45:56 +0300

    Added Stand-Alone emulator project by template

---
+++ src/Clients/SAE/Zarem.SAE/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+// Avishai Dernis 2026
+
+using Microsoft.UI.Xaml;
+
+namespace Zarem.SAE;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
