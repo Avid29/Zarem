af92682e4dc8154e55d11b469030d117728623e3
Avishai Dernis <avid29@live.com>
2026-05-04 04:48:10 +0300

    Added strings for RISC-V rounding modes

--- src/Archs/RiscV/Zarem.RiscV.Base/Models/Instructions/Enums/Functions/RiscVRoundingMode.cs
+++ src/Archs/RiscV/Zarem.RiscV.Base/Models/Instructions/Enums/Functions/RiscVRoundingMode.cs
@@ -1,20 +1,23 @@
 ﻿// Avishai Dernis 2026
 
+using System.Text.Json.Serialization;
+
 namespace Zarem.Models.Instructions.Enums.Functions;
 
 /// <summary>
 /// An enum representing the rounding mode for floating-point instructions in RISC-V.
 /// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum RiscVRoundingMode : byte
 {
 #pragma warning disable CS1591
 
-    RoundToNearest = 0b000,
-    RoundTowardsZero = 0b001,
-    RoundDown = 0b010,
-    RoundUp = 0b011,
-    RoundToNearestMaxMagnitude = 0b100,
-    Dynamic = 0b111,
+    [JsonStringEnumMemberName("rne")] RoundToNearest = 0b000,
+    [JsonStringEnumMemberName("rtz")] RoundTowardsZero = 0b001,
+    [JsonStringEnumMemberName("rdn")] RoundDown = 0b010,
+    [JsonStringEnumMemberName("rup")] RoundUp = 0b011,
+    [JsonStringEnumMemberName("rmm")] RoundToNearestMaxMagnitude = 0b100,
+    [JsonStringEnumMemberName("dyn")] Dynamic = 0b111,
 
 #pragma warning restore CS1591
 }
