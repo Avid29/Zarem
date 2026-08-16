8432035d7f251b1cfeb5fe82be8be363153d386b
Avishai Dernis <avid29@live.com>
2026-08-16 04:08:24 +0300

    Added test for parsing and printing version info

--- src/Archs/RiscV/Zarem.RiscV.Base/Models/Versioning/RiscVExtensionInfo.cs
+++ src/Archs/RiscV/Zarem.RiscV.Base/Models/Versioning/RiscVExtensionInfo.cs
@@ -158,16 +158,27 @@ public readonly partial struct RiscVExtensionInfo : IParsable<RiscVExtensionInfo
         var impliedMisa = RiscVExtensions.None;
         var impliedZ = RiscVZExtensions.None;
 
-        // If G is present, it replaces IMAFD
+        // Handle determining I vs G
         var gDependencies = _dependencyMap["G"];
         bool isG = Contains(gDependencies);
-        if (isG)
-        {
-            sb.Append('G');
+        sb.Append(isG ? 'G' : 'I');
 
-            // Remove flags covered by G to avoid double printing
-            impliedMisa |= gDependencies.MisaFlags;
-            impliedZ |= gDependencies.ZFlags;
+        foreach (var (alias, dependencies) in _dependencyMap)
+        {
+            _extensionMap.TryGetValue(alias, out var flag);
+            _zExtensionMap.TryGetValue(alias, out var zFlag);
+            var extension = new RiscVExtensionInfo(flag, zFlag);
+
+            if (Contains(extension) && Contains(dependencies))
+            {
+                // Append the extension only if it's a pure alias
+                if (extension.MisaFlags is 0 && extension.ZFlags is 0)
+                    sb.Append(alias);
+
+                // Track implied dependencies
+                impliedMisa |= dependencies.MisaFlags;
+                impliedZ |= dependencies.ZFlags;
+            }
         }
 
         var printMisa = (MisaFlags & ~impliedMisa) | RiscVExtensions.Integers;
@@ -176,7 +187,8 @@ public readonly partial struct RiscVExtensionInfo : IParsable<RiscVExtensionInfo
         // Append remaining Standard Extensions
         foreach (var (name, flag) in _sortedExtensions)
         {
-            if (name == "I" && isG)
+            // I/G is already handled
+            if (name is "I" or "G")
                 continue;
 
             if (flag is not 0 &&
@@ -186,7 +198,7 @@ public readonly partial struct RiscVExtensionInfo : IParsable<RiscVExtensionInfo
             }
         }
 
-        // Append Multi-letter extensions with '+'
+        // Append Multi-letter extensions with '_'
         bool firstZ = true;
         foreach (var (name, flag) in _sortedZExtensions)
         {
---
+++ tests/Archs/RiscV/Test.RiscV/RiscVVersionParsingTests.cs
@@ -0,0 +1,19 @@
+﻿// Avishai Dernis 2026
+
+using Test.Archs;
+using Zarem.Models.Versioning;
+
+namespace Test.RiscV;
+
+[TestClass]
+public class RiscVVersionParsingTests : VersionParsingTests
+{
+    [DataTestMethod]
+    [DataRow("RV32I")]
+    [DataRow("RV32G")]
+    [DataRow("RV32ICD")]
+    [DataRow("RV64I")]
+    [DataRow("RV64G")]
+    [DataRow("RV64ICD")]
+    public void TestRiscVVersion(string version) => ParsePrintTest<RiscVVersionInfo>(version);
+}
