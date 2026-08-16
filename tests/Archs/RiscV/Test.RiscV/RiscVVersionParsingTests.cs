557116f877a163a61cb55d0ec6d8e29987e7c3ed
Avishai Dernis <avid29@live.com>
2026-08-16 04:24:09 +0300

    Fixed reparsing usage in RISC-V tests

--- tests/Archs/RiscV/Test.RiscV/RiscVVersionParsingTests.cs
+++ tests/Archs/RiscV/Test.RiscV/RiscVVersionParsingTests.cs
@@ -11,9 +11,9 @@ public class RiscVVersionParsingTests : VersionParsingTests
     [DataTestMethod]
     [DataRow("RV32I")]
     [DataRow("RV32G")]
-    [DataRow("RV32ICD", false)]
+    [DataRow("RV32ICD", true)]
     [DataRow("RV64I")]
     [DataRow("RV64G")]
-    [DataRow("RV64ICD", false)]
+    [DataRow("RV64ICD", true)]
     public void TestRiscVVersion(string version, bool reparse = false) => ParsePrintTest<RiscVVersionInfo>(version, reparse);
 }
